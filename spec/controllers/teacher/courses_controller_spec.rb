ca03fd2401da0f95edb6d9553982e81f3a74f886
Eric Allen <eric@apprentice.io>
2014-01-22 10:26:31 -0700

    User can edit course
    Refactored course edit, new views to use _form partial

---
+++ spec/controllers/teacher/courses_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Teacher::CoursesController do
+
+  pending 'GET edit' do
+
+    context 'when signed in as Teacher' do
+      it 'renders the Course edit page' do
+
+      end
+    end
+  end
+end
