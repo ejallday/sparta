8bee6dea557e62ba67ba057cf0aa7e23b724be3f
Eric Allen <eric@apprentice.io>
2014-01-23 11:04:36 -0700

    refactor form text field submission

--- spec/features/teacher/adding_an_assignment_spec.rb
+++ spec/features/teacher/adding_an_assignment_spec.rb
@@ -23,11 +23,11 @@ feature 'teacher adding an assignment' do
 
 
     select 'Science', from: :assignment_course_id
-    fill_in :assignment_name, with: 'Pop Quiz'
-    fill_in :assignment_description, with: 'I hope you studied!'
+    fill_in_text_field(:assignment, :name, 'Pop Quiz')
+    fill_in_text_field(:assignment, :description, 'I hope you studied!')
     select_date( :assignment, :assigned_on, assigned_on )
     select_date( :assignment, :due_on, due_on )
-    fill_in :assignment_points_possible, with: 100
+    fill_in_text_field(:assignment, :points_possible, 100)
     click_button I18n.t('helpers.submit.create', model: 'Assignment')
 
     expect(current_path).to eq(teacher_assignments_path)
@@ -39,7 +39,9 @@ feature 'teacher adding an assignment' do
     expect(page).to have_content('points possible: 100')
   end
 
-
+  def fill_in_text_field(prefix, field, value)
+    fill_in :"#{prefix}_#{field}", with: value  
+  end
 
   def select_date(prefix, field, date)
     select date.year, from: :"#{prefix}_#{field}_1i"
