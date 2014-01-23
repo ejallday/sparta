952a7be19ac339d0324aaaba31246ecfdf93cdff
Eric Allen <eric@apprentice.io>
2014-01-23 10:39:20 -0700

    refactor form date selection

--- spec/features/teacher/adding_an_assignment_spec.rb
+++ spec/features/teacher/adding_an_assignment_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 feature 'teacher adding an assignment' do
+  
   scenario 'by navigating to new assignment page' do
     teacher = create(:user)
     visit teacher_dashboard_path(as: teacher)
@@ -10,31 +11,39 @@ feature 'teacher adding an assignment' do
 
   scenario 'can create an assignment with valid attributes' do
     teacher = create(:user)
-    assignment_date = Date.parse('January 15, 2014')
-    assignment_due = Date.parse('January 17, 2014')
 
     %w(Math Science History Quantum\ Physics).each do |course_name|
       create(:course, name: course_name)
     end
+
     visit new_teacher_assignment_path(as: teacher)
+
+    assigned_on = DateTime.parse('January 15, 2014')
+    due_on = DateTime.parse('January 17, 2014')
+
+
     select 'Science', from: :assignment_course_id
     fill_in :assignment_name, with: 'Pop Quiz'
-    fill_in :assignment_description, with: 'test test test'
-    select '2014', from: :assignment_assigned_on_1i
-    select 'January', from: :assignment_assigned_on_2i
-    select '15', from: :assignment_assigned_on_3i
-    select '2014', from: :assignment_due_on_1i
-    select 'January', from: :assignment_due_on_2i
-    select '17', from: :assignment_due_on_3i
+    fill_in :assignment_description, with: 'I hope you studied!'
+    select_date( :assignment, :assigned_on, assigned_on )
+    select_date( :assignment, :due_on, due_on )
     fill_in :assignment_points_possible, with: 100
     click_button I18n.t('helpers.submit.create', model: 'Assignment')
 
     expect(current_path).to eq(teacher_assignments_path)
     expect(page).to have_content('course: Science')
     expect(page).to have_content('name: Pop Quiz')
-    expect(page).to have_content('description: test test test')
-    expect(page).to have_content("date assigned: #{I18n.l(assignment_date, format: :long)}")
-    expect(page).to have_content("date due: #{I18n.l(assignment_due, format: :long)}")
+    expect(page).to have_content('description: I hope you studied!')
+    expect(page).to have_content('assigned on: January 15, 2014')
+    expect(page).to have_content('due on: January 17, 2014')
     expect(page).to have_content('points possible: 100')
+  end
+
+
+
+  def select_date(prefix, field, date)
+    select date.year, from: :"#{prefix}_#{field}_1i"
+    select date.strftime('%B'), from: :"#{prefix}_#{field}_2i"
+    select date.day, from: :"#{prefix}_#{field}_3i"
   end
 end
