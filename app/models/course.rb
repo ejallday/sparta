c21b21dc4a6d4abb9a80309ceb2f1f05cdb88d81
Eric Allen <eric@apprentice.io>
2014-02-03 08:44:30 -0700

    Course Instructor delegates methods to Teacher

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,14 @@
 class Course < ActiveRecord::Base
   belongs_to :teacher
 
+  def instructor_first_name
+    teacher.first_name
+  end
+
+  def instructor_full_name
+    teacher.full_name
+  end
+
   def instructor_last_name
     teacher.last_name
   end
