1abf9a9c7987cf3053a1e99990c55d245fb65839
Eric Allen <eric@apprentice.io>
2014-02-24 10:54:57 -0700

    Add Courses, Guardians, Students to Admin Dash

--- spec/features/admin/manages_courses_spec.rb
+++ spec/features/admin/manages_courses_spec.rb
@@ -10,6 +10,33 @@ feature 'admin manages courses', js: true do
     expect(current_path).to eq(new_admin_course_path)
   end
 
+  scenario 'by navigating courses page' do
+    admin = create(:user, :admin)
+
+    visit admin_dashboard_path(as: admin)
+
+    click_link t('admin.dashboards.courses')
+    expect(current_path).to eq(admin_courses_path)
+  end
+
+  scenario 'by navigating guardians page' do
+    admin = create(:user, :admin)
+
+    visit admin_dashboard_path(as: admin)
+
+    click_link t('admin.dashboards.guardians')
+    expect(current_path).to eq(admin_guardians_path)
+  end
+
+  scenario 'by navigating students page' do
+    admin = create(:user, :admin)
+
+    visit admin_dashboard_path(as: admin)
+
+    click_link t('admin.dashboards.students')
+    expect(current_path).to eq(admin_students_path)
+  end
+
   scenario 'fill out new course form' do
     admin = create(:user, :admin)
     teacher = create(:teacher, first_name: 'Duce', last_name: 'Bigelow')
