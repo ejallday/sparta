74b11f9aa056a2eaf62d44866edd99660a336b1f
Eric Allen <eric@apprentice.io>
2014-02-28 17:00:41 -0700

    Define behavior lists
    Also refactored behavior submission based on which behavior button is
    pushed.

--- app/assets/javascripts/student_action.js.coffee
+++ app/assets/javascripts/student_action.js.coffee
@@ -19,6 +19,6 @@ studentActionFactory = (RailsResource) ->
       _.include(@studentIds, studentId)
 
     isValid: =>
-      @name && !_.isEmpty(@studentIds)
+      !_.isEmpty(@studentIds)
 
 app.factory('StudentAction', ['RailsResource', studentActionFactory])
--- app/models/student_action.rb
+++ app/models/student_action.rb
@@ -1,9 +1,12 @@
 class StudentAction < ActiveRecord::Base
+  POSITIVE_BEHAVIORS = ['Quality Work', 'Participation']
+  NEGATIVE_BEHAVIORS = ['Swearing', 'Calling out']
+
   belongs_to :enrollment
   has_one :student, through: :enrollment
 
   validates :enrollment, presence: true
-  validates :name, presence: true
+  validates :name, inclusion: { in: POSITIVE_BEHAVIORS + NEGATIVE_BEHAVIORS }
 
   delegate :id, to: :student, prefix: true
 
